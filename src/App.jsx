7c8d5ef8c944af6e6d611e5a5264a604184d1177
ckiroshan <ckiroshan@gmail.com>
2025-08-31 15:08:52 +0530

    feat: add configuration files & UI components
    - Introduced components.json for component configuration & aliases.
    - Added jsconfig.json for module path resolution.
    - Updated package.json & package-lock.json with new dependencies including Radix UI components & utility libraries.
    - Created Button & Switch components with styling using Tailwind CSS & class variance authority.
    - Enhanced App component to utilize new UI components.
    - Updated index.css to include Tailwind CSS & custom styles.
    - Implemented utility functions for class name management in utils.js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import Button from './Button';
+import { Button } from "./components/ui/button";
+import { Switch } from "./components/ui/switch";
+
+function handleClick() {
+  console.log("Button Clicked");
+}
 
 function App() {
   return (
     <div className="border-2 border-black m-5 text-center p-3">
-      <Button/>
+      <Button onClick={handleClick}>Click Me</Button>
+      <Switch></Switch>
     </div>
   );
 }

068a6ec872c0a3be3af9aada7f579a750a178f61
ckiroshan <ckiroshan@gmail.com>
2025-08-31 15:15:40 +0530

    refactor: App component to display hotel cards and integrate hotel data.
    - Added HotelCard component for hotel details.
    - Created data.js for hotel information.
    - Updated User component to include a button with an onClick event.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import HotelCard from "./HotelCard";
 import { Button } from "./components/ui/button";
-import { Switch } from "./components/ui/switch";
-
-function handleClick() {
-  console.log("Button Clicked");
-}
+import { hotels } from "./data";
 
 function App() {
   return (
-    <div className="border-2 border-black m-5 text-center p-3">
-      <Button onClick={handleClick}>Click Me</Button>
-      <Switch></Switch>
+    <div className="py-5 border-2 border-black m-5 p-3">
+      {hotels.map((hotel, i) => {
+        return (
+          <HotelCard
+            key={hotel._id}
+            name={hotel.name}
+            location={hotel.location}
+            rating={hotel.rating}
+            reviews={hotel.reviews}
+            price={hotel.price}
+          />
+        );
+      })}
+      <Button onClick={() => console.log("Clicked")}>Click Me</Button>
     </div>
   );
 }
